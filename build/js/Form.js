4bad7e67b68761bda1952e276516befc39975b5f
Horror <rawwar999@gmail.com>
2018-02-26 21:14:55 +0200

    Project copy to build

--- build/js/Form.js
+++ build/js/Form.js
@@ -2,7 +2,6 @@ var menuOpen = document.querySelector('.head__menu');
 var controlMenu = document.querySelector('.menu-header');
 var menuClose = document.querySelector('.menu__close');
 
-
 menuOpen.addEventListener("click", function(event) {
   event.preventDefault();
   controlMenu.classList.remove('menu__header--close');
